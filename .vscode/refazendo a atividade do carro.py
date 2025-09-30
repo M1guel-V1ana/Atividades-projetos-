48b9ae0235fdb19cad7a68eced56a6fc22f25a8e
miguel <vianamiguelangelo@gmail.com>
2025-09-30 11:31:37 -0300

    ola

--- .vscode/refazendo a atividade do carro.py
+++ .vscode/refazendo a atividade do carro.py
@@ -1,45 +1,46 @@
 class Carro:
-    def __init__(self, passa, gs, km ):
-        self.passa : int = passa
-        self.gs : int = gs
-        self.km : int = km
+    def __init__(self, passa: int = 0, gas: int = 0, km: int = 0):
+        self.passa = passa
+        self.gas = gas
+        self.km = km
 
-    def __str__(self):
-        return f"pass : {self.passa}, gas: {self.gs}, km: {self.km}"
+    def __str__(self) -> str:
+        return f"pass: {self.passa}, gas: {self.gas}, km: {self.km}"
 
-
-    def Maxpass(self):
+    def maxPassa(self):
         if self.passa < 2:
             self.passa += 1
         else:
-            print("perigo! Limite de pessoas atingido ")
+            print("fail: limite de pessoas atingido")
 
     def leave(self):
         if self.passa == 0:
-            print("ops! Não tem niguém no carro")
+            print("fail: nao ha ninguem no carro")
         else:
             self.passa -= 1
 
-    def Maxga(self, increment : int):
-        self.gs += increment
-        if self.gs >100:
-            self.gs = 100
+    def gasMax(self, increment: int):
+        self.gas += increment
+        if self.gas > 100:
+            self.gas = 100
 
-
-    def drive(self, distance : int):
-        if self.pas == 0:
-            print("ops! cadê a galera do carro")
-        elif self.gs == 0:
-            print("cadê a gasosa ?")
-
+    def drive(self, distance: int):
+        if self.passa == 0:
+            print("fail: nao ha ninguem no carro")
+        elif self.gas == 0:
+            print("fail: tanque vazio")
         else:
-            self.km += distance
-            self.gas -= distance
-
+            if distance > self.gas:
+                print(f"fail: tanque vazio apos andar {self.gas} km")
+                self.km += self.gas
+                self.gas = 0
+            else:
+                self.km += distance
+                self.gas -= distance
 
 
 def main():
-    carro = Carro("", "", "")
+    carro = Carro()
 
     while True:
         line = input()
@@ -49,15 +50,17 @@ def main():
         if args[0] == "end":
             break
         elif args[0] == "enter":
-            carro.Maxpass()
+            carro.maxPassa()
         elif args[0] == "show":
             print(carro)
         elif args[0] == "leave":
             carro.leave()
         elif args[0] == "fuel":
             increment = int(args[1])
-            carro.Maxga(increment)
+            carro.gasMax(increment)
         elif args[0] == "drive":
             increment = int(args[1])
             carro.drive(increment)
+
+
 main()
---
+++ .vscode/refazendo mais uma vez a atividade do carro.py
@@ -0,0 +1,2 @@
+class Carro:
+    def __str__(self, passa :int, km : int, gs: int ):
