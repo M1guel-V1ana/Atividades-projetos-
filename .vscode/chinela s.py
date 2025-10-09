0d97a1647262fc200644d88a727520180dd94a46
miguel <vianamiguelangelo@gmail.com>
2025-10-08 21:00:33 -0300

    oiooo

--- .vscode/chinela s.py
+++ .vscode/chinela s.py
@@ -1,12 +1,21 @@
 class Chinela:
     def __init__(self):
         self.__tamanho : int = 0
-    def getTamanho(selfself):
+    def getTamanho(self):
         return self.__tamanho
     def setTamanho(self, valor: int) -> int :
         if valor > 19 and valor < 51:
-            self.__tamnaho = valor
+            self.__tamanho = valor
             return
         else:
             print("não existe esse tamanho de chinela")
+            return
 
+def main():
+    chinelin = Chinela()
+    while chinelin.getTamanho() == 0 :
+        tamanho = int(input(":"))
+        chinelin.setTamanho(tamanho)
+    print("parabens, você comprou um chinelo de tamanho", chinelin.getTamanho())
+
+main()
