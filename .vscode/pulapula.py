c630f9839479fce6d225241707a406ae6bb69e0c
Miguel <miguel@gmail.com>
2025-11-13 00:33:52 -0300

    iniciando pula pula

---
+++ .vscode/pulapula.py
@@ -0,0 +1,12 @@
+class Kid:
+    def __init__(self, age: int, name: str):
+        self.age = age
+        self.name = name
+
+    def getAge(self):
+        return self.age
+
+    def getName(self):
+        return self.name
+
+    def setAge(self, ):
